dea8d994fa770c2685c55599b976eac21e496372
Ayan2027 <ayandhal2014@gmail.com>
2025-12-18 18:11:45 +0530

    Add Cancel and Save changes button

--- client/src/components/ProfileModel.jsx
+++ client/src/components/ProfileModel.jsx
@@ -110,7 +110,12 @@ const ProfileModal = ({setShowEdit}) => {
                <input type="text" className='w-full p-3 border border-gray-200 rounded-lg' placeholder='Please enter your location' onChange={(e)=>setEditForm({...editForm, location: e.target.value})} value={editForm.location}/>
             </div>
 
-            
+            <div className='flex justify-end space-x-3 pt-6'>
+
+                <button onClick={()=> setShowEdit(false)} type='button' className='px-4 py-2 border border-gray-300 rounded-lg text-gray-700 hover:bg-gray-50 transition-colors cursor-pointer'>Cancel</button>
+
+                <button type='submit' className='px-4 py-2 bg-gradient-to-r from-indigo-500 to-purple-600 text-white rounded-lg hover:from-indigo-600 hover:to-purple-700 transition cursor-pointer'>Save Changes</button>
+            </div>
 
             </form>
         </div>
